3f0e2d739dbbfde092034a2ca0308d9e582da666
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-09-13 10:08:16 +0700

    Update bai tap

---
+++ 20230911-Java-Slot07-Lab06/Part-02-Exercise-01/src/main/java/org/example/Client.java
@@ -0,0 +1,40 @@
+package org.example;
+
+/**
+ * Write a description of class Client here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Client {
+    /**
+     * Constructor for objects of class Client
+     */
+    public Client() {
+        // To do:
+    }
+
+    public void doUnchecked(String value) {
+        // Can phai check exception, neu khong --> bug
+        int result=canThrowUncheckedException(value);
+        System.out.println("result="+result);
+    }
+
+    private int canThrowUncheckedException(String value) {
+        return Integer.parseInt(value);
+    }
+
+    public void doChecked() {
+        try {
+            // Buoc phai check exceptions o day! Khong cach nao khac
+            canThrowCheckedException();
+            System.out.println("OK");
+        }catch(Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    private int canThrowCheckedException() throws Exception{
+        throw new Exception("Failure");
+    }
+}
---
+++ 20230911-Java-Slot07-Lab06/Part-02-Exercise-02/src/UncheckedException.java
@@ -0,0 +1,28 @@
+/**
+ * Write a description of class Client here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class UncheckedException{
+    /**
+     * Constructor for objects of class Client
+     */
+    public UncheckedException () {
+        // To do:
+    }
+
+    public static void main(String[] args) {
+        int i, n = 2;
+        int a[] = new int[n];
+
+        //Declare Scanner Object named input
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        for(i=0; i<=n; i++)
+        {
+            System.out.printf("a[%d] = ", i);
+            a[i] = input.nextInt();
+        }
+    }
+}
